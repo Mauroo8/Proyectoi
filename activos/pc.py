f6f77bb6e53f8053fc7a531fca48bccdb08a2ffb
Mauroo8 <mauromendieta246@gmail.com>
2026-03-20 14:40:02 -0300

    correccion pc ghz

--- activos/pc.py
+++ activos/pc.py
@@ -1,10 +1,11 @@
 from .equipo import Equipo
 
 class Pc(Equipo):
-    def __init__(self, id=None, marca=None, modelo=None, estado="disponible", ubicacion=None, fecha_compra=None, procesador=None, ram_gb=None, disco_gb=None, 
+    def __init__(self, id=None, marca=None, modelo=None, estado="disponible", ubicacion=None, fecha_compra=None, procesador=None, ghz=None, ram_gb=None, disco_gb=None, 
                  tipo_equipo="escritorio"):
         super().__init__(id, marca, modelo, estado, ubicacion, fecha_compra)
         self.procesador = procesador
+        self.ghz = ghz
         self.ram_gb = ram_gb
         self.disco_gb = disco_gb
         self.tipo_equipo = tipo_equipo
